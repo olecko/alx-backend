a62f8c016c78d9ddde770e36e3eaf09df2960eaf
Paul Michael <paulebi4eva@gmail.com>
2023-10-31 22:16:24 +0000

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
-
-
+""" Basic Flask app, Basic Babel setup, Get locale from request,
+    Parametrize templates """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+""" instantiate the Babel object """
 
 
 class Config(object):
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """ config class """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# configure the flask app
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
+""" Use that class as config for Flask app """
+
+
+@app.route('/')
+def root():
+    """ basic Flask app """
+    return render_template("3-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """ to determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """_summary_
-    """                                                         
-    return render_template('3-index.html')
-
-
-if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run()
